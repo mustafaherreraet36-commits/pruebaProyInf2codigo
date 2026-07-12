07c2883212a43eb42c102d8c11a25bbf5a5e79e8
mustafaherreraet36-commits <mustafaherrera.et36@gmail.com>
2026-07-12 18:36:14 -0300

    Delete showcaseAPI-main directory

--- showcaseAPI-main/README.md
+++
@@ -1,1 +0,0 @@
-<img width="1548" height="1040" alt="image" src="https://github.com/user-attachments/assets/d64bd4a8-2985-4a14-be9b-b295829bcdc7" />
--- showcaseAPI-main/routes/hobbyRoutes.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express');
-const { getHobbies, createHobby } = require('../controllers/hobbyController');
-
-const router = Router();
-
-router.get('/', getHobbies);
-router.post('/', createHobby);
-
-module.exports = router;
